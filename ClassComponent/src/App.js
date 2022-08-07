46eee62d7a8f9cbb1eea348a1be1b3692dafbbd2
Maria Carolina Xavier <mcs_nasc@hotmail.com>
2022-08-07 11:46:44 -0300

    React

---
+++ ClassComponent/src/App.js
@@ -0,0 +1,42 @@
+import logo from './logo.svg';
+import './App.css';
+import React from "react";
+import {Component} from 'react';
+import banana from './assets/banana.jpg';
+ /*
+function oi() {
+  return (<h1>Olá mundo</h1>
+  );
+}
+*/
+class Pessoa extends Component{
+
+  state ={
+    pessoa:[{
+    nome: 'Gabriel',
+    idade: 44,
+    comida: 'ovo',
+    musica: ['eletronica', 'funk', 'sertaneja']
+    }]};
+
+    render(){
+      return(
+        <div>
+          <h1>{this.state.pessoa[0].nome}</h1>
+          <h2>{this.state.pessoa[0].idade}</h2>
+          <h3>{this.state.pessoa[0].comida}</h3>
+          <ul>
+            <li>{this.state.pessoa[0].musica[0]}</li>
+            <li>{this.state.pessoa[0].musica[1]}</li>
+            <li>{this.state.pessoa[0].musica[2]}</li>
+          </ul>
+          <img src={banana}/>
+        </div>
+        
+      )
+    }
+}
+
+
+
+export default Pessoa;
