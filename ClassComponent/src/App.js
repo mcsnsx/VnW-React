ebdcff724b23382f49a64bf13066664622103ef2
Maria Carolina Xavier <mcs_nasc@hotmail.com>
2022-08-07 11:48:08 -0300

    Class Component

--- ClassComponent/src/App.js
+++ ClassComponent/src/App.js
@@ -1,42 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
 import React from "react";
-import {Component} from 'react';
-import banana from './assets/banana.jpg';
- /*
-function oi() {
-  return (<h1>Olá mundo</h1>
-  );
-}
-*/
-class Pessoa extends Component{
+import { Component } from "react";
 
-  state ={
-    pessoa:[{
-    nome: 'Gabriel',
-    idade: 44,
-    comida: 'ovo',
-    musica: ['eletronica', 'funk', 'sertaneja']
-    }]};
-
-    render(){
-      return(
-        <div>
-          <h1>{this.state.pessoa[0].nome}</h1>
-          <h2>{this.state.pessoa[0].idade}</h2>
-          <h3>{this.state.pessoa[0].comida}</h3>
-          <ul>
-            <li>{this.state.pessoa[0].musica[0]}</li>
-            <li>{this.state.pessoa[0].musica[1]}</li>
-            <li>{this.state.pessoa[0].musica[2]}</li>
-          </ul>
-          <img src={banana}/>
-        </div>
-        
-      )
-    }
+class Frase extends Component {
+  render() {
+    return (
+      <div>
+        <p>Olá, mundo!</p>
+      </div>
+    );
+  }
 }
 
-
-
-export default Pessoa;
+export default Frase;
